5dcce38d5a00889fce9a3e9d6bb66644c60554d0
UnqKode <mycodeunq@gmail.com>
2025-07-15 23:38:36 +0530

    Third Commit

--- src/Components/Info.jsx
+++ src/Components/Info.jsx
@@ -9,7 +9,15 @@ function Info() {
       data-scroll-section
       className="relative z-20 min-h-screen bg-white px-4 md:px-12 py-16 overflow-hidden"
     >
-      
+      <motion.img
+        src={Sp}
+        alt="Background"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ duration: 1 }}
+        className="absolute hidden md:block left-0 top-0 h-full object-cover z-0 pointer-events-none"
+        loading="lazy"
+      />
       <div className="grid grid-cols-1 md:grid-cols-10 gap-8 p-24 z-20" >
         {/* Tech Stack - 3/10 columns (30%) */}
         <div className="md:col-span-4 flex items-start justify-start">
